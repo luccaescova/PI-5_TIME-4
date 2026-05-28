539683e507d4784460aa554c44f855e35470d7fc
luccaescova <160538429+luccaescova@users.noreply.github.com>
2026-05-28 11:10:07 -0300

    Revert " sistema de recomendação de livros por tags"
    This reverts commit 204c84fc977100c3abcd22c528fdfa24cb493dd3.

--- PI-5_TIME-4-Backend/src/routes/help.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendHelpEmail } from "../controllers/helpController";
-
-const router = Router();
-
-router.post("/send", sendHelpEmail);
-
-export default router;
